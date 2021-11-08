38e81c70c3fd75ebbf6813188f0de6a27bb08d92
fredericdesibert <freddie@revmo.app>
2021-11-08 21:09:27 +0000

    add (failing) test

--- test/test_parse_led.py
+++ test/test_parse_led.py
@@ -7,7 +7,7 @@ class TestPyLedTest:
     def test_row_to_binary(self):
         test_row = '         0x00, 0x00, 0x00, 0x60, 0x60, 0x20, 0x40,  // Code for char ,'
         expected_result = ['00000000', '00000000', '00000000', '01100000', '01100000', '00100000', '01000000']
-        
+
         test_output = parse_led.row_to_binary_str(test_row)
 
         assert expected_result == test_output
@@ -33,10 +33,10 @@ class TestPyLedTest:
     def test_char_to_matrix(self):
         expected_output_a = numpy.matrix([
             [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 1, 1, 1, 0, 0, 0, 0],
             [1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 0, 0, 1, 0, 0, 0], 
+            [1, 0, 0, 0, 1, 0, 0, 0],
             [1, 0, 0, 1, 1, 0, 0, 0],
             [0, 1, 1, 0, 1, 0, 0, 0]
         ])
@@ -63,6 +63,22 @@ class TestPyLedTest:
         test_output = parse_led.matrix_rewrite_serpentine(test_matrix)
 
         assert (expected_output == test_output).all()
-    
+
     def test_matrix_to_pixel_list(self):
-        pass
+        fg = parse_led.GRB_Pixel(255, 0, 0)
+        bg = parse_led.GRB_Pixel(0, 0, 128)
+
+        test_matrix_small = numpy.matrix([
+            [1, 0, 1],
+            [1, 0, 0],
+            [0, 1, 1],
+        ])
+
+        expected_output_small = [
+            fg, fg, bg, bg,
+            bg, fg, bg, bg,
+            fg, bg. fg. bg
+        ]
+
+        test_output_small = parse_led.matrix_to_pixel_list(test_matrix_small, foreground=fg, background=bg, serpentine=True)
+        assert expected_output_small == test_output_small
