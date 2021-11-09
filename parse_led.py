4a155b389b1ce2ece5fa7ec33a8d2fb07f598d07
Freddie de Sibert <freddie@revmo.app>
2021-11-09 10:03:15 +0000

    fix boundaries for left/right scrolling, remove VERY slow astuple call

--- parse_led.py
+++ parse_led.py
@@ -5,6 +5,7 @@ from enum import Enum
 import board
 import neopixel
 import numpy
+import cProfile
 
 # These are used in setup
 def translate_header_file(filename: str = "FontMatrise.h") -> list[list[int]]:
@@ -62,7 +63,7 @@ class GRB_Pixel:
         return 3
 
     def __iter__(self):
-        return iter(astuple(self))
+        return iter((self.green, self.red, self.blue))
 
 
 def _handle_row(row: list[str]) -> str:
@@ -99,7 +100,6 @@ def matrix_to_pixel_list(
     background: GRB_Pixel = GRB_Pixel(0, 0, 0),
     serpentine: bool = True,
 ) -> list[GRB_Pixel]:
-
     zero_row = numpy.zeros(matrix.shape[1], dtype=int)
     with_bottom_row = numpy.row_stack((matrix, zero_row))
 
@@ -108,6 +108,7 @@ def matrix_to_pixel_list(
 
     ravelled = with_bottom_row.ravel('F')
     as_list = ravelled.tolist()[0]
+    
     as_pixels = [(background, foreground)[x] for x in as_list]
 
     return as_pixels
@@ -118,13 +119,12 @@ def scroll_text(
     LED_WIDTH: int,
     LED_HEIGHT: int,
     scroll_direction: ScrollDirection,
-    scroll_speed: int = 20,
     serpentine: bool = True,
     pixels_per_char: int = 8,
-    emulate: bool = False,
     brightness: float = 0.01,
     foreground: GRB_Pixel = GRB_Pixel(255, 0, 0),
     background: GRB_Pixel = GRB_Pixel(0, 0, 0),
+    framerate: int = 40
 ):
     import board
     import neopixel
@@ -133,15 +133,22 @@ def scroll_text(
     message_matrix = string_to_matrix(message)
     pixels = neopixel.NeoPixel(board.D21, num_pixels, brightness=brightness)
 
-    left_pad, right_pad = 2 * (numpy.matrix(numpy.zeros((LED_HEIGHT, LED_WIDTH), dtype=int)),)
+    left_pad, right_pad = 2 * (numpy.matrix(numpy.zeros((LED_HEIGHT-1, LED_WIDTH), dtype=int)),)
     padded_matrix = numpy.concatenate((left_pad, message_matrix, right_pad),1)
-    for i in range(padded_matrix.shape[1]):
-        left_bound = i
-        right_bound = (i + LED_WIDTH)
+    # Reverse the boundaries if scrolling right
+    boundaries = (0, padded_matrix.shape[1])[::scroll_direction.value]
+    scroll_start, scroll_end = boundaries
+
+    # Scroll through the text
+    for i in range(scroll_start, scroll_end, scroll_direction.value):
+        a_bound = i
+        b_bound = i + (scroll_direction.value * LED_WIDTH)
+        left_bound = min(a_bound, b_bound)
+        right_bound = max(a_bound, b_bound)
         display = padded_matrix[:, left_bound:right_bound]
         display_pixels = matrix_to_pixel_list(display, foreground=foreground, background=background, serpentine=True)
-        pixels[0: num_pixels - 1] = display_pixels
-        time.sleep(0.1)
+        pixels[0: len(display_pixels) - 1] = display_pixels
+        time.sleep(0.65 / framerate) # compensate for compute time
 
 def display_text(
     message: str,
@@ -161,5 +168,8 @@ def display_text(
     pixels[0:num_pixels-1] = message_pixels[0:num_pixels]
 
 if __name__ == "__main__":
-    hw = "abcdefg"
-    scroll_text(hw, 32, 8, serpentine=True, brightness=0.1, scroll_direction=ScrollDirection.LEFT)
+    hw = "This is a very quick test message"
+    #cProfile.run(
+    #    'scroll_text(hw, 96, 8, serpentine=True, brightness=0.1, scroll_direction=ScrollDirection.LEFT)'
+    #)
+    scroll_text(hw, 96, 8, serpentine=True, brightness=0.1, scroll_direction=ScrollDirection.LEFT)
